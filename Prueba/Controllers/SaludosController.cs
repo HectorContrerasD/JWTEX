540099c612fc9be4975646282edc1a98b703ae95
HectorContrerasD <201g0239@rcarbonifera.tecnm.mx>
2024-05-07 17:10:36 -0600

    Add project files.

---
+++ Prueba/Controllers/SaludosController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Prueba.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class SaludosController : ControllerBase
+    {
+        public IActionResult get()
+        {
+            return Ok("Hola mundo");
+        }
+    }
+}
